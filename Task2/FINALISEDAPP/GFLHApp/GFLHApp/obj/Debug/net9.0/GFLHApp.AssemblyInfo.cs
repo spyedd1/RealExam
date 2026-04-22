28c7d783532ac95a97c92dfc4a3dc73e7a0bc00f
spyedd1 <jamiegallagher0607@gmail.com>
2026-04-22 16:47:45 +0100

    Test Log and Dev Diary

--- Task2/FINALISEDAPP/GFLHApp/GFLHApp/obj/Debug/net9.0/GFLHApp.AssemblyInfo.cs
+++ Task2/FINALISEDAPP/GFLHApp/GFLHApp/obj/Debug/net9.0/GFLHApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GFLHApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329a18500be42996cd5c1bd2fc735ff3e83f3991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0720f377d94fd017a683e3e95bdd944228d9cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GFLHApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GFLHApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
